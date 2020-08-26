642199e2794294f1800a38e184f6894cedf3f34d
dudash <jason.dudash@gmail.com>
2020-08-26 18:34:42 -0400

    committing missing files - ugly but going to dump some missing updates in here...

--- code/app-ui/app.js
+++ code/app-ui/app.js
@@ -103,7 +103,7 @@ if (FAKE_USER === true || FAKE_USER == "true") {
   app.use(auth.checkSso(), function (req, res, next) {
     var authenticated = 'Check SSO Success (' + (req.session['keycloak-token'] ? 'Authenticated' : 'Not Authenticated') + ')'
     debugSSO(authenticated)
-    if (req.session['keycloak-token'] ? true : false) {
+    if ((req.session['keycloak-token'] ? true : false) && req.kauth.grant.access_token != null) {
       res.locals.username = req.kauth.grant.access_token.content.name
       res.locals.useremail = req.kauth.grant.access_token.content.email
       res.locals.userId = req.kauth.grant.access_token.content.preferred_username
